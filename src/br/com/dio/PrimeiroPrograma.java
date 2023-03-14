87b27d78b154463b1cc4b831a159edd737f5ef5e
jedualmeida <joseeduardoalmeida@live.com>
2023-03-13 21:27:19 -0300

    criando class calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -12,6 +12,43 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro Livro1 = new Livro("The Witcher", 300);
+
+        System.out.println(Livro1);
+
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+}
